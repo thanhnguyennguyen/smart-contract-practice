34241c79b6e624bf151c1ccac050978d8a368778
NguyenNguyen <thanhnguyen.nguyen@vn.zalora.com>
2018-07-14 10:59:25 +0700

    Updating provider

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3')
-const web3 = new Web3(ganache.provider());
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const {interface, bytecode} = require('../compile');
 let accounts;
 let inbox;
@@ -14,6 +16,7 @@ beforeEach(async() => {
   .deploy({data: bytecode, arguments: ['Hello Solidity']})
   .send({from: accounts[0], gas: '1000000'})
 
+  inbox.setProvider(provider);
 });
 
 describe('Get Accounts', () => {
