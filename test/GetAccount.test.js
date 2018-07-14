a396939c4083ac94b2e4d92d9f09e5e06dac53c9
NguyenNguyen <thanhnguyen.nguyen@vn.zalora.com>
2018-07-14 09:43:14 +0700

    Getting list accounts to deploy a contract from web3 lib

---
+++ test/GetAccount.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  // get a list of all accounts
+  web3.eth.getAccounts()
+  .then(
+    fetchedAccounts => {
+      console.log(fetchedAccounts);
+    }
+  );
+
+  // use one of those accounts to deploy the contracts
+
+});
+
+describe('Get Accounts', () => {
+  it('deploy a contract', () => {})
+});
