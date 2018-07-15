2ea4c410e817f480319ae2ca2b520b90ee707afe
NguyenNguyen <thanhnguyen.nguyen@vn.zalora.com>
2018-07-15 20:59:02 +0700

    Lottery contract: enter function implementation

--- contracts/lottery/code/lottery.sol
+++ contracts/lottery/code/lottery.sol
@@ -2,8 +2,14 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     constructor () public {
-        manager = msg.sender;
+      manager = msg.sender;
+    }
+
+    function enter() public payable {
+      require(msg.value > .01 ether);
+      players.push(msg.sender);
     }
 }
