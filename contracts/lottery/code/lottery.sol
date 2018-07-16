2dec9f15f984dfa2aa58b12fb2b3c4798b8c11ef
NguyenNguyen <thanhnguyen.nguyen@vn.zalora.com>
2018-07-16 11:59:00 +0700

    Lottery contract: More useful function

--- contracts/lottery/code/lottery.sol
+++ contracts/lottery/code/lottery.sol
@@ -8,15 +8,13 @@ contract Lottery {
       manager = msg.sender;
     }
 
-    function getPlayers() public view returns(address[]) {
-      return players;
-    }
-
+    /* enter a candidate to the current lottery */
     function enter() public payable {
       require(msg.value > .01 ether);
       players.push(msg.sender);
     }
 
+    /* manager calls to pick a winner and reset game */
     function pickWinner() public restricted {
       uint index = random() % players.length;
       players[index].transfer(address(this).balance);
@@ -25,6 +23,22 @@ contract Lottery {
       players = new address[](0);
     }
 
+    /* get current balance of contract */
+    function getCurrentBalance() public view returns(uint) {
+      return address(this).balance;
+    }
+
+    /* get number of player */
+    function getNumberOfPlayer() public view returns(uint) {
+      return players.length;
+    }
+
+    /* get players */
+    function getPlayers() public view returns(address[]) {
+      return players;
+    }
+
+    /* pseudo random */
     function random() private view returns(uint) {
       return uint(sha256(abi.encodePacked(block.difficulty, now, players)));
     }
