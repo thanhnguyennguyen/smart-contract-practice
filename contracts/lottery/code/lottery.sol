9f6a6022d41df3bb46de6618fa271606949edf1c
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-05 22:23:37 +0700

    adding return data for pickWinner

--- contracts/lottery/code/lottery.sol
+++ contracts/lottery/code/lottery.sol
@@ -1,54 +1,55 @@
-pragma solidity ^0.4.17;
+pragma solidity ^ 0.4 .17;
 
 contract Lottery {
-    address public manager;
-    address[] public players;
+  address public manager;
+  address[] public players;
 
-    constructor () public {
-      manager = msg.sender;
-    }
+  constructor() public {
+    manager = msg.sender;
+  }
 
-    /* enter a candidate to the current lottery */
-    function enter() public payable {
-      require(msg.value > .01 ether);
-      players.push(msg.sender);
-    }
+  /* enter a candidate to the current lottery */
+  function enter() public payable {
+    require(msg.value > .01 ether);
+    players.push(msg.sender);
+  }
 
-    /* manager calls to pick a winner and reset game */
-    function pickWinner() public restricted {
-      uint index = random() % players.length;
-      players[index].transfer(address(this).balance);
+  /* manager calls to pick a winner and reset game */
+  function pickWinner() public restricted returns(address) {
+    uint index = random() % players.length;
+    players[index].transfer(address(this).balance);
+    address winner = players[index];
+    //  reset players
+    players = new address[](0);
+    return winner;
+  }
 
-      //  reset players
-      players = new address[](0);
-    }
+  /* get current balance of contract */
+  function getCurrentBalance() public view returns(uint) {
+    return address(this).balance / (1 ether);
+  }
 
-    /* get current balance of contract */
-    function getCurrentBalance() public view returns(uint) {
-      return address(this).balance / (1 ether);
-    }
+  /* get number of player */
+  function getNumberOfPlayer() public view returns(uint) {
+    return players.length;
+  }
 
-    /* get number of player */
-    function getNumberOfPlayer() public view returns(uint) {
-      return players.length;
-    }
+  /* get players */
+  function getPlayers() public view returns(address[]) {
+    return players;
+  }
 
-    /* get players */
-    function getPlayers() public view returns(address[]) {
-      return players;
-    }
+  /* pseudo random */
+  function random() private view returns(uint) {
+    return uint(sha256(abi.encodePacked(block.difficulty, now, players)));
+  }
 
-    /* pseudo random */
-    function random() private view returns(uint) {
-      return uint(sha256(abi.encodePacked(block.difficulty, now, players)));
-    }
+  modifier restricted() {
+    // check if it is called by manager
+    require(msg.sender == manager);
+    // make sure players is not an empty list
+    require(players.length > 0);
 
-    modifier restricted() {
-      // check if it is called by manager
-      require(msg.sender == manager);
-      // make sure players is not an empty list
-      require(players.length > 0);
-
-      _;
-    }
+    _;
+  }
 }
