1da78d58ce43d959e78e76403cd62572e5cfb2ce
Nguyen Nguyen <nguyenbk92@gmail.com>
2018-08-12 17:40:27 +0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -25,8 +25,7 @@ const deploy = async() => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account ', accounts[0]);
     const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({data: '0x' + bytecode})
-      .send({gas: '1000000', from: accounts[0]});
+      .new({data: '0x' + bytecode, gas: '1000000', from: accounts[0]});
       console.log(interface);
       console.log('Contract deployed to ', result.options.address);
   } catch (err) {
